0613b82f557e2476ed84cbddfdec763e0e255467
Greg Aiello <gregoryaiello693@gmail.com>
2022-01-13 00:20:03 -0500

    Fix crossOrigin warning from Next.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html className="h-full">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400&display=optional" rel="stylesheet" />
         </Head>
         <body className="h-full">
