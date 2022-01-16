453bc1dfb08d90469e167071262f0d5ad318454f
Greg Aiello <gregoryaiello693@gmail.com>
2022-01-16 14:07:15 -0500

    Add styles and link to ScheduleBar component

--- components/ScheduleBar/ScheduleBar.js
+++ components/ScheduleBar/ScheduleBar.js
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import ChevronRight from '../ChevronRight/ChevronRight';
+
 export default function ScheduleBar(props) {
   const wrapperClasses = [
     'w-full',
@@ -6,20 +9,32 @@ export default function ScheduleBar(props) {
     '-translate-x-1/2',
     'mx-auto',
     'text-center',
-    'py-5',
     'fixed',
     'bottom-0',
-    'bg-white ',
-    'border-t-2 ',
-    'border-x-2 ',
-    'shadow-md ',
+    'bg-white',
+    'hover:bg-slate-100',
+    'border-t-2',
+    'border-x-2',
+    'shadow-md',
     'transition-all',
     'duration-300',
+    'font-bold',
+    'text-lg',
   ];
 
   if (props.trackedShows === null || !props.trackedShows.length) {
     wrapperClasses.push('translate-y-full');
   }
 
-  return <div className={wrapperClasses.reduce((prev, current) => `${prev} ${current}`)}>Tracked Shows <span className="py-1 px-3 font-bold bg-red-700 text-white rounded-full">{props?.trackedShows?.length || '0'}</span></div>;
+  return (
+    <div className={wrapperClasses.reduce((prev, current) => `${prev} ${current}`)}>
+      <Link href="/subscription-url">
+        <a className="block py-5 px-2">
+          Tracked Shows
+          <span className="py-1 px-3 ml-2 font-bold bg-red-700 text-white rounded-full">{props?.trackedShows?.length || '0'}</span>
+          <ChevronRight />
+        </a>
+      </Link>
+    </div>
+  );
 }
