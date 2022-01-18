22e72eecbda231820c0768dc55069377d72dc3f3
Greg Aiello <gregoryaiello693@gmail.com>
2022-01-18 17:52:42 -0500

    Add custom server.js with proper redirects for production environment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const { createServer } = require('https');
+const { createServer: createHttpsServer } = require('https');
+const { createServer: createHttpServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 const fs = require('fs');
@@ -7,18 +8,35 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: fs.readFileSync('./localcerts/localhost.key'),
-  cert: fs.readFileSync('./localcerts/localhost.crt'),
-};
+if (dev) {
+  const httpsOptions = {
+    key: fs.readFileSync('./localcerts/localhost.key'),
+    cert: fs.readFileSync('./localcerts/localhost.crt'),
+  };
 
-app.prepare().then(() => {
-  createServer(httpsOptions, (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+  app.prepare().then(() => {
+    createHttpsServer(httpsOptions, (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
 
-  }).listen(3000, err => {
-    if (err) throw err;
-    console.log('> Ready on https://localhost:3000');
+    }).listen(3000, err => {
+      if (err) throw err;
+      console.log('> Ready on https://localhost:3000');
+    });
   });
-});
+} else {
+  app.prepare().then(() => createHttpServer((req, res) => {
+    const { headers, url } = req;
+    if (!headers['x-forwarded-proto'] || headers['x-forwarded-proto'] !== 'https' || headers.host.indexOf('www.') === 0) {
+      res.writeHead(301, { Location: `https://airtime.wtf/${url}` });
+      res.end();
+      return {};
+    }
+
+    const parsedUrl = parse(url, true);
+    return handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on localhost:3000');
+  }));
+}
